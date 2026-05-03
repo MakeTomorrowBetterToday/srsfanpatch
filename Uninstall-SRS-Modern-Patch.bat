d24a01ba53b78191c6db13c69985cc7733cb1e8d
MakeTomorrowBetterToday <steamdream@f-m.fm>
2026-05-02 17:38:09 -0700

    Delete Uninstall-SRS-Modern-Patch.bat

--- Uninstall-SRS-Modern-Patch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Uninstall-SRS-Modern-Patch.ps1"
-if errorlevel 1 (
-  echo.
-  echo Uninstall failed.
-  pause
-)
